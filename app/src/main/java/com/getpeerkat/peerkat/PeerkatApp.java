d4565a6d7bc532a0a8568a4e6fa676b4ea3620dc
Dusko <duskox@gmail.com>
2016-02-20 19:58:24 +0100

    Project start

---
+++ app/src/main/java/com/getpeerkat/peerkat/PeerkatApp.java
@@ -0,0 +1,9 @@
+package com.getpeerkat.peerkat;
+
+import android.app.Application;
+
+/**
+ * Created by dvorkapic on 18/02/16.
+ */
+public class PeerkatApp extends Application {
+}
---
+++ common/src/main/java/Constants.java
@@ -0,0 +1,7 @@
+/**
+ * Created by dvorkapic on 18/02/16.
+ */
+public class Constants {
+
+    public static String APP_NAME = "Peerkat";
+}
